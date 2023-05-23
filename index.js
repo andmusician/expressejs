f1b06921198471dc4cdaad5b8f146f0555494075
Anderson de Oliveira Lima <andersonlima_guitar@hotmail.com>
2023-05-23 09:59:13 -0300

    Passando parametros do Backend para o front utilizando EJS

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/:nome?/:lang?", (req, res) => {
+  var nome = req.params.nome;
+  var language = req.params.lang;
+  res.render("index", {
+    nome: nome,
+    lang: language,
+    empresa: "Seven",
+    inscritos: 8000,
+  });
 });
 
 let port = 8080;
