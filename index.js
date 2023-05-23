b8f3b67310ece3e07dc405c41b34fe415522fdef
Anderson de Oliveira Lima <andersonlima_guitar@hotmail.com>
2023-05-23 17:25:25 -0300

    Foreach no EJS

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ app.get("/:nome?/:lang?", (req, res) => {
   var nome = req.params.nome;
   var language = req.params.lang;
   var exibirmsg = true;
+  var produtos = [
+    { nome: "Doritos", preco: 9.19 },
+    { nome: "Pepsi", preco: 4.59 },
+    { nome: "Whey Protein", preco: 59.0 },
+  ];
   res.render("index", {
     nome: nome,
     lang: language,
     empresa: "Seven",
     inscritos: 8000,
     msg: exibirmsg,
+    produtos: produtos,
   });
 });
 
